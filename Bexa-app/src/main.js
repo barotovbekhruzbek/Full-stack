1b7c549302af196de092c656de8280aa6896897a
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2022-12-31 10:33:38 +0500

    changed all file

--- Bexa-app/src/main.js
+++ Bexa-app/src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import uiComponent from "./ui-component";
+import store from "./store";
 
 
 const app = createApp(App)
 uiComponent.map(component => app.component(component.name, component))
 
 app.use(router)
-
+app.use(store)
 app.mount('#app')
