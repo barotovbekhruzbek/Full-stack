d40780617cd81982bf7772130f3a5603ee781980
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-01-26 14:47:42 +0500

    add

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="d-flex flex-column flex-md-row align-items-center py-3 mb-4 border-bottom">
+		<a class="d-flex align-items-center text-dark text-decoration-none">
+			<img :src="logo" alt="logo" style="width: 100px; cursor: pointer" @click="toHomeHandler" />
+		</a>
+
+		<nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+      <template v-if="isLoggedIn">
+        <RouterLink :to="{name: 'login'}" class="me-3 py-2 text-dark text-decoration-none"
+        >{{ user.username }}</RouterLink
+        >
+      </template>
+      <template v-if="!isLoggedIn">
+        <RouterLink :to="{name: 'login'}" class="me-3 py-2 text-dark text-decoration-none"
+        >Login</RouterLink
+        >
+        <RouterLink :to="{name: 'register'}" class="me-3 py-2 text-dark text-decoration-none"
+        >Register</RouterLink
+        >
+      </template>
+
+		</nav>
+	</div>
+
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import {logo} from '../contstants'
+export default {
+	data() {
+		return {
+			logo,
+		}
+	},
+  computed : {
+    ...mapState({
+      user: state => state.auth.user,
+      isLoggedIn: state => state.auth.isLoggedIn
+    })
+  },
+	methods: {
+		toHomeHandler() {
+			return this.$router.push({name: 'home'})
+		},
+	},
+}
+</script>
+
+<style></style>
---
+++ src/components/Register.vue
@@ -0,0 +1,61 @@
+<template>
+	<main class="form-signin w-25 m-auto mt-5">
+		<form>
+			<img :src="logo" alt="logo" style="width: 100px; cursor: pointer" @click="toHomeHandler" />
+			<h1 class="h3 mb-3 fw-normal mt-3">Register</h1>
+
+			<ValidationError v-if="validationErrors" :validationErrors="validationErrors" />
+
+			<Input :label="'Name'" :type="'text'" v-model="username" />
+			<Input :label="'Email address'" :type="'email'" v-model="email" />
+			<Input :label="'Password'" :type="'password'" v-model="password" />
+
+			<Button type="submit" :disabled="isLoading" @click="submitHandler">Register</Button>
+		</form>
+	</main>
+</template>
+
+<script>
+import {logo} from '../contstants'
+import ValidationError from '@/components/ValidationError.vue'
+export default {
+	data() {
+		return {
+			logo,
+			username: '',
+			email: '',
+			password: '',
+		}
+	},
+	components: {
+		ValidationError,
+	},
+	computed: {
+		isLoading() {
+			return this.$store.state.auth.isLoading
+		},
+		validationErrors() {
+			return this.$store.state.auth.errors
+		},
+	},
+	methods: {
+		submitHandler(e) {
+			e.preventDefault()
+			const data = {
+				username: this.username,
+				email: this.email,
+				password: this.password,
+			}
+			this.$store
+				.dispatch('register', data)
+				.then(user => {
+					console.log('USER', user)
+					this.$router.push({name: 'home'})
+				})
+				.catch(err => console.log('ERROR', err))
+		},
+	},
+}
+</script>
+
+<style></style>
