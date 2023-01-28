160a607fde345a801a8ba6836b8ae1477759fcbc
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-01-28 11:01:20 +0500

    article card

--- Bexa-app/src/views/HomeView.vue
+++ Bexa-app/src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
 	<div>
-		<h1>Home</h1>
-    
+
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+      <ArticleCard v-for="article in data" :article="article" :key="article.id" />
+    </div>
+
 	</div>
 </template>
 
 <script>
+import ArticleCard from "../components/ArticleCard.vue";
 import {mapState} from "vuex";
 
 export default {
-
+components : {
+  ArticleCard
+},
   computed: {
     ...mapState({
       data: state => state.articles.data,
